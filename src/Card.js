3b116fbd180a35742b72b0aef76443ed4b5e27f0
Brian Bagrov <Brianbagrov@gmail.com>
2020-06-25 20:02:48 -0400

    MVP attained.

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,39 @@
 import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: rgb(61, 61, 61);
+	line-height: 2.5rem;
+	font-size: 1.5rem;
+  }`;
+
+const StyledCard = styled.div`
+	text-align: center;
+	background-color: #242424;
+	color: white;
+	border: 1px solid black;
+	border-radius: 50px;
+	width: 50%;
+	margin-top: 2%;
+	height: 90%;
+`;
+
+const StyledImg = styled.img`
+	width: 80%;
+	border-radius: 25px;
+	margin: 2.5%;
+	max-height: 50vh;
+`;
 
 const Card = (props) => {
 	return (
-		<div className='card'>
+		<StyledCard>
 			<div>Title: {props.data.title}</div>
 			<div>Date: {props.data.date}</div>
-			<img src={props.data.hdurl} alt='Unavailable.' />
+			<StyledImg src={props.data.url} alt='Unavailable.' />
 			<div>Explanation: {props.data.explanation}</div>
-		</div>
+		</StyledCard>
 	);
 };
 
